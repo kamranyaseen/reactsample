94f890d87baac6668abb23bc2c5fa756d17c3459
Kamran Yaseen <kamranmyaseen@gmail.com>
2018-01-21 16:00:42 +0300

    CRUD Operation Using React, Nodejs, Express, MongoDb
    React is a front-end library developed by Facebook. It's used for handling front end for web and mobile apps. ReactJS allows us to create reusable UI components.
    Node is an open source server framework.It is used to develop I/O intensive web applications like video streaming sites, single-page applications, and other web applications.
    Express is a web framework for Node.js. It is a fast, robust and asynchronous in nature.
    MongoDB is a No SQL database. It is an open-source, cross-platform, document-oriented database written in C++.

---
+++ config.js
@@ -0,0 +1,8 @@
+var mongo = require("mongoose");  
+var db = mongo.connect("mongodb://localhost:27017/reactcrud", function(err, response){  
+   if(err){ console.log('Failed to connect to ' + db); }  
+   else{ console.log('Connected to ' + db, ' + ', response); }  
+});  
+  
+  
+module.exports =db;  
